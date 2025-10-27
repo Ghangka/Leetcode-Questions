b2c1659a84f81ea182b67315486f752f5f88eb62
Ghangka <ghangka.s@gmail.com>
2025-10-27 15:03:28 -0400

    solved group anagrams question

---
+++ group_anagrams.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/group-anagrams/
+# O(n * m) time complexity
+# O(n) space complexity
+
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+
+            for c in s:
+                # mapping characters to index in alphabet
+                count[ord(c) - ord('a')] += 1
+            # lists can't be keys for dictionaries (lists are mutable), so we convert to tuple
+            res[tuple(count)].append(s)
+
+        # return only the values of the dictionary
+        return list(res.values())
