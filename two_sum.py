6f5b44079c7c046f77503489d74d76171fb82ef1
Ghangka <ghangka.s@gmail.com>
2025-10-27 14:58:10 -0400

    solved TwoSum question

---
+++ two_sum.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/two-sum/ 
+# O(n) time complexity
+# O(n) space complexity   
+
+class Solution:
+    def twoSum(self, nums:list[int], target:int) -> list[int]:
+
+        prevMap = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
