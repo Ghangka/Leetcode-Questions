fca5d2ab2563952a3bbb3fe992afee58e34b8eff
Ghangka <ghangka.s@gmail.com>
2025-11-17 14:53:24 -0500

    changed function name

--- number_of_islands.py
+++ number_of_islands.py
@@ -7,7 +7,7 @@ import collections
 from typing import List
 
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
+    def num_islands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
 
