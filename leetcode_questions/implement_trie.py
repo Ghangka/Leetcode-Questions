b3330c0f022b2f26c38cd488db49770e4b8e4748
Ghangka <ghangka.s@gmail.com>
2025-12-01 15:38:24 -0500

    solved implement trie/prefix tree

---
+++ leetcode_questions/implement_trie.py
@@ -0,0 +1,40 @@
+# https://neetcode.io/problems/implement-prefix-tree/question?list=blind75
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    # Runtime: O(n)
+    # Space: O(n)
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.is_end_of_word = True
+
+    # Runtime: O(n)
+    # Space: O(1)
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return curr.is_end_of_word
+
+    # Runtime: O(n)
+    # Space: O(1)
+    def starts_with(self, prefix: str) -> bool:
+        curr = self.root
+        for c in prefix:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return True
