723ebdb85452fea4b679e91e0cf240c2d83c2cd8
Ghangka <ghangka.s@gmail.com>
2026-01-26 14:46:45 -0500

    solved Combination Sum

---
+++ leetcode_questions/combination_sum.py
@@ -0,0 +1,25 @@
+# https://neetcode.io/problems/combination-target-sum/question
+
+# Runtime: O(2^t) where t is the target
+# Space: O(t) where t is the target
+
+from typing import List
+
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        def dfs(i, cur, total):
+            if total == target:
+                res.append(cur.copy())
+                return
+            if i >= len(nums) or total > target:
+                return
+
+            cur.append(nums[i])
+            dfs(i, cur, total + nums[i])
+            cur.pop()
+            dfs(i + 1, cur, total)
+
+        dfs(0, [], 0)
+        return res
